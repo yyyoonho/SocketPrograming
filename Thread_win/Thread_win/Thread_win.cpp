0c6c5a2a9deff726e9a7ac98ba79a4b39d8be26d
yyyoonho <parn997@naver.com>
2024-06-13 21:48:52 +0900

    feat: 커널 오브젝트 signaled상태 확인 (WaitForSingleObject)

--- Thread_win/Thread_win/Thread_win.cpp
+++ Thread_win/Thread_win/Thread_win.cpp
@@ -29,18 +29,28 @@ unsigned WINAPI ThreadFunc(void* arg)
 int main()
 {
     HANDLE hThread;
+    DWORD wr;
     unsigned threadID;
     int param = 5;
 
     hThread = (HANDLE)_beginthreadex(NULL, 0, ThreadFunc, (void*)&param, 0, &threadID);
-
     if (hThread == 0)
     {
         cout << "Error: _beginthreadex()" << endl;
         return 1;
     }
 
-    Sleep(3000);
+    // WaitForSingleObject
+    // 하나의 커널 오브젝트에 대해서 signaled 상태인지를 확인하기 위해서 호출하는 함수
+    // OS: "프로세스나 쓰레드가 종료되면 해당 커널 오브젝트를 signaled 상태로 변경해 놓겠다!"
+    if ((wr = WaitForSingleObject(hThread, INFINITE)) == WAIT_FAILED)
+    {
+        cout << "Error: WaitForSingleObject()" << endl;
+        return 1;
+    }
+
+    string tmp = (wr == WAIT_OBJECT_0) ? "Signaled" : "Time-out";
+    cout << "Aait result: " << tmp << endl;
 
     return 0;
 }
