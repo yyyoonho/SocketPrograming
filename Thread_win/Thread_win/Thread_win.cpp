3a24b1148d131a7f5fac5502c4073b6b6b1762f8
yyyoonho <parn997@naver.com>
2024-06-17 18:43:47 +0900

    feat: Event(Manual Mode) 동기화

--- Thread_win/Thread_win/Thread_win.cpp
+++ Thread_win/Thread_win/Thread_win.cpp
@@ -5,6 +5,7 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 
+#include <Windows.h>
 #include <process.h>    // C++ 11 표준 Thread 함수
 #include <thread>       // WinAPI Thread 함수
 
@@ -15,36 +16,42 @@
 
 using namespace std;
 
-#define NUM_THREAD 50
-long long num = 0;
-long long sum = 0;
+#define STR_LEN 100
+char str[STR_LEN];
 
-HANDLE sem1;
-HANDLE sem2;
+// Event 동기화
+// Mutex, Semaphore는 Auto-Reset 모드
+// Event는 Auto-Reset/ Manual-Reset 모드 중 하나를 선택할 수 있다.
+HANDLE hEvent;
 
-unsigned WINAPI Read(void* arg)
+unsigned WINAPI NumberOfA(void* arg)
 {
-    for (int i = 0; i < 5; i++)
+    int cnt = 0;
+
+    WaitForSingleObject(hEvent, INFINITE);
+    for (int i = 0; str[i] != 0; i++)
     {
-        WaitForSingleObject(sem2, INFINITE);
-        cout << "Input num: ";
-        cin >> num;
-        ReleaseSemaphore(sem1, 1, NULL);
+        if (str[i] == 'A')
+            cnt++;
     }
+
+    cout << "Num of A: " << cnt << endl;
 
     return 0;
 }
 
-unsigned WINAPI Accu(void* arg)
+unsigned WINAPI NumberOfOthers(void* arg)
 {
-    for (int i = 0; i < 5; i++)
+    int cnt = 0;
+
+    WaitForSingleObject(hEvent, INFINITE);
+    for (int i = 0; str[i] != 0; i++)
     {
-        WaitForSingleObject(sem1, INFINITE);
-        sum += num;
-        ReleaseSemaphore(sem2, 1, NULL);
+        if (str[i] != 'A')
+            cnt++;
     }
 
-    cout << "Result: " << sum << endl;
+    cout << "Num of Others: " << cnt << endl;
 
     return 0;
 }
@@ -54,17 +61,24 @@ int main()
     HANDLE hThread1;
     HANDLE hThread2;
 
-    sem1 = CreateSemaphore(NULL, 0, 1, NULL);
-    sem2 = CreateSemaphore(NULL, 1, 1, NULL);
+    // 두번째 인자 TRUE -> Manual Mode
+    // 세번째 인자 FALSE -> Non-signaled 상태로 초기화
+    hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
 
-    hThread1 = (HANDLE)_beginthreadex(NULL, 0, Read, NULL, 0, NULL);
-    hThread2 = (HANDLE)_beginthreadex(NULL, 0, Accu, NULL, 0, NULL);
+    hThread1 = (HANDLE)_beginthreadex(NULL, 0, NumberOfA, NULL, 0, NULL);
+    hThread2 = (HANDLE)_beginthreadex(NULL, 0, NumberOfOthers, NULL, 0, NULL);
+    
+    cout << "Input String: ";
+    cin >> str;
+
+    SetEvent(hEvent); // NS->S
 
     WaitForSingleObject(hThread1, INFINITE);
     WaitForSingleObject(hThread2, INFINITE);
 
-    CloseHandle(sem1);
-    CloseHandle(sem2);
+    ResetEvent(hEvent); // S->NS;
+
+    CloseHandle(hEvent);
 
     return 0;
 }
