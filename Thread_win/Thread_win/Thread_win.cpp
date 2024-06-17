a88ca75c3858e1aa4a4423e96827a53f517d6ed1
yyyoonho <parn997@naver.com>
2024-06-17 17:06:12 +0900

    feat: Semaphore 동기화

--- Thread_win/Thread_win/Thread_win.cpp
+++ Thread_win/Thread_win/Thread_win.cpp
@@ -17,51 +17,54 @@ using namespace std;
 
 #define NUM_THREAD 50
 long long num = 0;
+long long sum = 0;
 
-mutex m1;
+HANDLE sem1;
+HANDLE sem2;
 
-unsigned WINAPI ThreadFuncInc(void* arg)
+unsigned WINAPI Read(void* arg)
 {
-    lock_guard<mutex> lock(m1);
-    for (int i = 0; i < 50000000; i++)
+    for (int i = 0; i < 5; i++)
     {
-        num -= 1;
+        WaitForSingleObject(sem2, INFINITE);
+        cout << "Input num: ";
+        cin >> num;
+        ReleaseSemaphore(sem1, 1, NULL);
     }
 
     return 0;
 }
 
-unsigned WINAPI ThreadFuncDes(void* arg)
+unsigned WINAPI Accu(void* arg)
 {
-    lock_guard<mutex> lock(m1);
-    for (int i = 0; i < 50000000; i++)
+    for (int i = 0; i < 5; i++)
     {
-        num += 1;
+        WaitForSingleObject(sem1, INFINITE);
+        sum += num;
+        ReleaseSemaphore(sem2, 1, NULL);
     }
+
+    cout << "Result: " << sum << endl;
 
     return 0;
 }
 
 int main()
 {
-    HANDLE tHandles[NUM_THREAD];
+    HANDLE hThread1;
+    HANDLE hThread2;
 
-    for (int i = 0; i < NUM_THREAD; i++)
-    {
-        if (i % 2)
-        {
-            tHandles[i] = (HANDLE)_beginthreadex(NULL, 0, ThreadFuncInc, NULL, 0, NULL);
-        }
-        else
-        {
-            tHandles[i] = (HANDLE)_beginthreadex(NULL, 0, ThreadFuncDes, NULL, 0, NULL);
-        }
-    }
+    sem1 = CreateSemaphore(NULL, 0, 1, NULL);
+    sem2 = CreateSemaphore(NULL, 1, 1, NULL);
 
+    hThread1 = (HANDLE)_beginthreadex(NULL, 0, Read, NULL, 0, NULL);
+    hThread2 = (HANDLE)_beginthreadex(NULL, 0, Accu, NULL, 0, NULL);
 
-    WaitForMultipleObjects(NUM_THREAD, tHandles, TRUE, INFINITE);
+    WaitForSingleObject(hThread1, INFINITE);
+    WaitForSingleObject(hThread2, INFINITE);
 
-    cout << "Result: " << num << endl;
+    CloseHandle(sem1);
+    CloseHandle(sem2);
 
     return 0;
 }
