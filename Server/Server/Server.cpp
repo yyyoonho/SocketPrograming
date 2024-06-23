2a96d5abcf4421362466da73bf3a633b6db324dd
yyyoonho <parn997@naver.com>
2024-06-23 15:14:12 +0900

    feat: Overlapped IO : Completion Routine

--- Server/Server/Server.cpp
+++ Server/Server/Server.cpp
@@ -18,6 +18,14 @@ using namespace std;
 
 #define BUF_SIZE 100
 
+WSABUF dataBuf;
+char buf[BUF_SIZE];
+unsigned long recvBytes = 0;
+
+void CALLBACK CompRoutine(DWORD dwError, DWORD szRecvBytes, LPWSAOVERLAPPED lpOverlapped, DWORD flags)
+{
+
+}
 
 int main(int argc, char* argv[])
 {
@@ -27,12 +35,9 @@ int main(int argc, char* argv[])
 
     int recvAddrSize;
     
-    WSABUF dataBuf;
     WSAEVENT evObj;
     WSAOVERLAPPED overlapped;
 
-    char buf[BUF_SIZE];
-    unsigned long recvBytes = 0;
     unsigned long flags = 0;
 
     // 라이브러리 초기화
@@ -70,30 +75,33 @@ int main(int argc, char* argv[])
     recvAddrSize = sizeof(recvAddr);
     hRecvSock = accept(hLisnSock, (SOCKADDR*)&recvAddr, &recvAddrSize);
 
-    evObj = WSACreateEvent();
-    memset(&overlapped, 0, sizeof(overlapped));
+    evObj = WSACreateEvent(); // Dummy event object (오로지 함수 호출을 위해 존재)
+    memset(&overlapped, 0, sizeof(overlapped)); 
     overlapped.hEvent = evObj;
     dataBuf.buf = buf;
     dataBuf.len = BUF_SIZE;
 
-    if (WSARecv(hRecvSock, &dataBuf, 1, &recvBytes, &flags, &overlapped, NULL) == SOCKET_ERROR)
+    // Dummy event object 여기에 활용!
+    if (WSARecv(hRecvSock, &dataBuf, 1, &recvBytes, &flags, &overlapped, CompRoutine) == SOCKET_ERROR)
     {
         if (WSAGetLastError() == WSA_IO_PENDING)
         {
             cout << "Background data receive..." << endl;
-
-            WSAWaitForMultipleEvents(1, &evObj, TRUE, WSA_INFINITE, FALSE);
-
-            WSAGetOverlappedResult(hRecvSock, &overlapped, &recvBytes, FALSE, NULL);
-        }
-        else
-        {
-            cout << "ERROR: WSARecv()" << endl;
-            return 1;
         }
     }
 
-    cout << "Received message: " << buf;
+    // 정말 Alertable Wait 상태 진입만을 위한 목적으로 함수호출
+    // Dummy event object 여기에 활용!
+    // Completion Routine으로 등록된 함수가 끝나면 리턴된다.
+    int idx = WSAWaitForMultipleEvents(1, &evObj, FALSE, WSA_INFINITE, TRUE);
+    if (idx == WAIT_IO_COMPLETION)
+    {
+        cout << "Overlapped IO Completed!" << endl;
+    }
+    else
+    {
+        cout << "ERROR: WSARecv()" << endl;
+    }
 
     WSACloseEvent(evObj);
     closesocket(hRecvSock);
